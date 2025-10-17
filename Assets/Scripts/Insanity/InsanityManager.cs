0cc1177b149f21d07e4a4397380853cb68be67b4
ZuperZlime <Rasmuswinterhag@gmail.com>
2025-10-17 19:21:03 +0200

    Insanity

---
+++ Assets/Scripts/Insanity/InsanityManager.cs
@@ -0,0 +1,53 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class InsanityEvent
+{
+    [SerializeField] string title;
+    public float insanityTriggerValue = 0;
+    public UnityEvent triggerd;
+    [HideInInspector] public bool hasTriggerd = false;
+    public void InvokeEvent()
+    {
+        if (!hasTriggerd)
+        {
+            triggerd?.Invoke();
+            hasTriggerd = true;
+        }
+    }
+}
+
+public class InsanityManager : MonoBehaviour
+{
+    public float insanity = 0;
+    [SerializeField] InsanityEvent[] insanityEvents;
+
+    void Update()
+    {
+        foreach (var @event in insanityEvents)
+        {
+            if (insanity >= @event.insanityTriggerValue)
+            {
+                @event.InvokeEvent();
+            }
+        }
+    }
+
+    public float GetInsanity()
+    {
+        return insanity;
+    }
+
+    public void ModifyInsanity(float value)
+    {
+        insanity += value;
+    }
+
+    public void DebugLog(string input)
+    {
+        Debug.Log(input);
+    }
+}
