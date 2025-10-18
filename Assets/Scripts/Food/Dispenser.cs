08fc43a357a2f8a686357b29a5dee8522018390a
Neo V. <neolgv78@gmail.com>
2025-10-18 20:26:44 +0200

    Add dispenser window animation and logic
    Introduced FadeIn and FadeOut animations and a Window animator controller for the dispenser window. Updated Game.unity to link the dispenser window GameObject and animator, and modified Dispenser.cs to trigger the window fade animation when dispensing completes.

--- Assets/Scripts/Food/Dispenser.cs
+++ Assets/Scripts/Food/Dispenser.cs
@@ -4,11 +4,14 @@ using UnityEngine.UI;
 public class Dispenser : MonoBehaviour
 {
     public bool isDispensing = false;
+    public GameObject dispenserWindow;
+    private FoodType foodType = FoodType.Soda;
     [SerializeField] private Image cupFillImage;
     private Animator animator;
     void Awake()
     {
         animator = this.GetComponent<Animator>();
+        cupFillImage.fillAmount = 0;
     }
     public void ToggleDispenser()
     {
@@ -21,6 +24,22 @@ public class Dispenser : MonoBehaviour
         if (isDispensing)
         {
             cupFillImage.fillAmount += Time.deltaTime * 0.5f;
+
+            if (cupFillImage.fillAmount >= 1f)
+            {
+                ToggleDispenser();
+                FoodReferenceTable.Instance.SpawnFood(foodType, transform.position);
+                
+                Invoke("Deactivate", 0.2f);
+            }
         }
+    }
+
+    void Deactivate()
+    {
+        isDispensing = false;
+        animator.SetBool("isDispensing", false);
+        cupFillImage.fillAmount = 0;
+        dispenserWindow.GetComponent<Animator>().SetTrigger("Fade");
     }
 }
